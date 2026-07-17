1183af78bc1ae11351ad7b3f354db49976a883d3
shravanneela <shravanneela756@gmail.com>
2026-07-17 23:33:12 +0530

    minimum force b/w balls

---
+++ force.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPossible(int[] position,int m,int k){
+        int prev=position[0];
+        m-=1;
+        for(int i=0;i<position.length;i++){
+            int val=position[i];
+            if(val-prev>=k){
+                m-=1;
+                prev=val;
+            }
+            if(m<=0){
+                return true;
+            }
+        }return false;
+    }
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int l=1;
+        int r=(int)Math.pow(10,9);
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isPossible(position,m,mid)){
+                 l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return r;
+    }
+}
